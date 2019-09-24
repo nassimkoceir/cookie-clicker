b40f3299ff46e12e7fcd0669199fb93684e921cf
Nassim Koceir <nassim.koceir@gmail.com>
2019-09-24 12:11:50 +0200

    display scorefunction

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,18 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 	let count = 0;
 	let multiplier = true;
 
-   const scoreLable = document.getElementById('score')
+  const scoreLable = document.getElementById('score');
 
-   document.getElementById('cookie').addEventListener('click', () => {
-      scoreLable.innerText = ++count
-   })
-
+  document.getElementById('cookie').addEventListener('click', () => {
+		++count;
+  })
 
 	function autoclick(){
 		if(multiplier == true){
-			 console.log(++count);
+			++count;
 		}
 	}
 
 	setInterval(autoclick, 1000);
+
+	function displayScore(){
+		scoreLable.innerHTML = count;
+	}
+
+	setInterval(displayScore, 200);
 })
