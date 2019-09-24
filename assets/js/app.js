c6f82816f2158c2265b2685827e3a59997795716
Nassim Koceir <nassim.koceir@gmail.com>
2019-09-24 11:40:30 +0200

    Autoclick

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-   let count = 0
+	let count = 0;
+	let multiplier = true;
    document.getElementById('cookie').addEventListener('click', () => {
-     
-      console.log(++count);
-      
-   })
-})
 
+      console.log(++count);
+
+   })
+
+	 function autoclick(){
+		 if(multiplier == true){
+			 console.log(++count);
+		 }
+	 }
+
+	 setInterval(autoclick, 1000);
+})
