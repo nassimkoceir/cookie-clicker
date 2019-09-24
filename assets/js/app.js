98bb5ad40e2928075ac42cf0a23a850ef11e1561
wailmadrane <wail.madrane@gmail.com>
2019-09-24 11:43:13 +0200

    adding score lable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
+// on page load
 document.addEventListener('DOMContentLoaded', () => {
+   
    let count = 0
+   const scoreLable = document.getElementById('score')
+
    document.getElementById('cookie').addEventListener('click', () => {
-     
-      console.log(++count);
-      
+      scoreLable.innerText = ++count
    })
+
+
 })
 
