967315a309e66d77e97695f0b04a7e35c8775f43
wailmadrane <wail.madrane@gmail.com>
2019-09-24 15:14:01 +0200

    adding n

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	let count = 0;
 	let multiplier = true;
+	let n = 2;
+	const multiPrice = 20;
 
-  const scoreLable = document.getElementById('score');
+	const scoreLable = document.getElementById('score');
 
-  document.getElementById('cookie').addEventListener('click', () => {
-		++count;
-  })
+	document.getElementById('cookie').addEventListener('click', () => {
+			count = count === 0 ? 1 : count + n;
+	})
 
 	function autoclick(){
 		if(multiplier == true){
@@ -19,9 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	setInterval(autoclick, 1000);
 
 	function displayScore(){
-      scoreLable.innerHTML = count;
-      requestAnimationFrame(displayScore)
+		scoreLable.innerHTML = count;
+		requestAnimationFrame(displayScore)
 	}
-   displayScore()
+	displayScore()
+	
+	
 
 })
